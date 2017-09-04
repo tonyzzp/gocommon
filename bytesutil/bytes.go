182de4be5d3732d0a64be17d800603c825cfbe89
tonyzzp <tonyzzp@qq.com>
2017-09-04 17:53:52 +0800

    bytes2int

--- bytesutil/bytes.go
+++ bytesutil/bytes.go
@@ -7,3 +7,18 @@ import (
 func Repeat(value int, times int) []byte {
 	return bytes.Repeat([]byte{byte(value)}, times)
 }
+
+func BytesToInt32(b [4]byte) int32 {
+	i := int32(int32(b[0])<<24 | int32(b[1])<<16 | int32(b[2])<<8 | int32(b[3]))
+	return i
+}
+
+func Int32ToBytes(i int32) []byte {
+	b := []byte{
+		byte(i >> 24),
+		byte(i >> 16),
+		byte(i >> 8),
+		byte(i),
+	}
+	return b
+}
